c6a41db1529d30b6f2e7a394be865304c65f789e
Nate Kent <ngkent75@gmail.com>
2021-05-26 17:44:55 -0400

    refactored javascript to have updated techniques, and implemented use of checkboxes in logic instead of prompts. Took out unnecessary while loop. All character types are guaranteed

--- assets/script.js
+++ assets/script.js
@@ -1,94 +1,87 @@
+const generatePassword = () => {
+  // array of lowercase alphabet called lowLetters
+  const letString = "abcdefghijklmnopqrstuvwxyz";
+  const lowLetters = "abcdefghijklmnopqrstuvwxyz".split("");
+
+  // array of uppercase alphabet called upLetters
+  const upString = letString.toUpperCase();
+  const upLetters = upString.split("");
+
+  // array of numbers called numbers
+  const numString = "0123456789";
+  const numbers = numString.split("");
+
+  // array for special characters called special
+  const special = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
+
+  // Empty array for total options for generator to choose from that values will be pushed into.
+  let options = []
+
+  // Identifies values of checked options and assignes them to variables
+  const lowLetterInc = document.getElementById('lowercase').checked
+  const upLetterInc = document.getElementById('uppercase').checked
+  const numberInc = document.getElementById('numbers').checked
+  const specialInc = document.getElementById('special').checked
+
+  // checks the length of password passed off of input selected
+  const passLength = document.getElementById('quantity').value
+
+  // Concats characters based on user's choices
+  if (lowLetterInc) {
+    options = options.concat(lowLetters);
+  }
+  if (upLetterInc) {
+    options = options.concat(upLetters);
+  }
+  if (numberInc) {
+    options = options.concat(numbers);
+  }
+  if (specialInc) {
+    options = options.concat(special);
+  }
+
+  // empty array where password will generate
+  let pass = [];
+
+  for (let i = 0; i < passLength; i++) {
+    pass.push(options[Math.floor(Math.random() * options.length)]);
+  };
+
+
+  // runs password generator until it includes all character types, ensuring at least one of each shows up, as long as the character is selected to be included in the password
+  if (((!pass.some(r => lowLetters.includes(r))) && (lowLetterInc)) ||
+    ((!pass.some(r => upLetters.includes(r))) && (upLetterInc)) ||
+    ((!pass.some(r => numbers.includes(r))) && (numberInc)) ||
+    ((!pass.some(r => special.includes(r))) && (specialInc))) {
+
+    console.log(`${pass} failed`);
+    generatePassword();
+    
+    return;
+  }
+
+  console.log(pass);
+
+  // turns pass array into a string so it looks like a proper password
+  password = pass.join("");
+
+};
+
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+const writePassword = () => {
   generatePassword();
+  // console.log(password);
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+  return;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-function generatePassword() {
-  // Prompt for user to choose lenth of password, assigned to variable passLenth
-  var passLength = prompt("How many characters? Please pick between 8 and 128.")
-  // Makes sure the user only picks between 8 and 128 characters
-  if (passLength >= 8 && passLength <= 128) {
-    
-    // variables to determine inclusion of each character
-    var lowLetterInc = confirm("Include lowercase letters?");
-    var upLetterInc = confirm("Include uppercase letters?");
-    var numberInc = confirm("Include numbers?");
-    var specialInc = confirm("Include special characters?");
-
-    // string of alphabet
-    var letString = "abcdefghijklmnopqrstuvwxyz";
-    // splits string of alphabet into an array
-    var lowLetters = "abcdefghijklmnopqrstuvwxyz".split("");
-
-    // Uppercase alphabet
-    var upString = letString.toUpperCase();
-    var upLetters = upString.split("");
-
-    // string of numbers
-    var numString = "0123456789";
-    // splits string of numbers into an array
-    var numbers = numString.split("");
-
-    // array for special characters
-    var special = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
-
-    // Empty array for total options for generator to choose from.
-    var options = [""]
-
-    // Concats characters based on user's choices
-    if (lowLetterInc) {
-      var options = options.concat(lowLetters);
-    }
-    if (upLetterInc) {
-      var options = options.concat(upLetters);
-    }
-    if (numberInc) {
-      var options = options.concat(numbers);
-    }
-    if (specialInc) {
-      var options = options.concat(special);
-    }
-
-    // empty array where password will generate
-    var pass = [];
-    // Assigns passL to passLenth so that the value assigned earlier can be used in the while loop without changing the variable.
-    passL = passLength;
-    // runs password generator until it includes all character types, ensuring at least one of each shows up, as long as the character is selected to be included in the password
-    while (((!pass.some(r => lowLetters.includes(r))) && (lowLetterInc)) ||
-      ((!pass.some(r => upLetters.includes(r))) && (upLetterInc)) ||
-      ((!pass.some(r => numbers.includes(r))) && (numberInc)) ||
-      ((!pass.some(r => special.includes(r))) && (specialInc))) {
-      // clears password to generate again
-      var pass = [];
-      // resets passL back to passLenth
-      passL = passLength;
-      // fills in characters into password
-      while (passL >= 1) {
-        pass.push(options[Math.floor(Math.random() * options.length)]);
-        passL--;
-      }
-    }
-
-    // turns pass array into a string so it looks like a proper password
-    password = pass.join("");
-
-  }
-  // When the user tries to select a password length that is not between 8 and 128, or is not a number.
-  else {
-
-    alert ("Please pick between 8 and 128.")
-    generatePassword()
-  }
-
-}
